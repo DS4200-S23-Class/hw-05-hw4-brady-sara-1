32784ea5b1942ce8c06fdda6ceffb6c3bc91595c
Sara Kent <kent.sa@northeastern.edu>
2023-02-20 13:22:02 -0500

    barplot done

--- main.js
+++ main.js
@@ -143,36 +143,85 @@ const FRAME2 = d3.select("#col1")
 						.attr("width", FRAME_WIDTH)
 						.attr("class", "frame");
 
-// read data from second file
-d3.csv("data/bar-data.csv").then((data) => {
+function build_plot() {
+	// read data from second file
+	d3.csv("data/bar-data.csv").then((data) => {
 
-	console.log(data);
+		console.log(data);
 
-	const MAX_Y2 = d3.max(data, (d) => {return parseInt(d.amount); });
+		const MAX_Y2 = d3.max(data, (d) => {return parseInt(d.amount); });
 
-	const X_SCALE2 = d3.scaleBand()
-						.domain(data.map((d) => {return d.category}))
-						.range([0, VIS_WIDTH]);
+		const X_SCALE2 = d3.scaleBand()
+							.domain(data.map((d) => {return d.category}))
+							.range([0, VIS_WIDTH]);
 
-	const Y_SCALE2 = d3.scaleLinear()
-						.range([VIS_HEIGHT, 0])
-						.domain([0, MAX_Y2])
+		const Y_SCALE2 = d3.scaleLinear()
+							.range([VIS_HEIGHT, 0])
+							.domain([0, MAX_Y2])
 
-	// plot
-	FRAME2.selectAll(".bar")
-					.data(data)
-	    			.enter().append("rect")
-	    						.attr("class", "bar")
-	    						.attr("x", d => {
-	    								return X_SCALE2(d.category) + MARGINS.left
-	    							})
-	    						.attr("y", d => {
-	    							return (Y_SCALE2(d.amount) + MARGINS.bottom)
-	    						})
-	    						.attr("width", X_SCALE2.bandwidth())
-	    						.attr("height", d => {
-	    							return (VIS_HEIGHT - Y_SCALE2(d.amount))
-	    						})
+		// plot
+		FRAME2.selectAll(".bar")
+						.data(data)
+		    			.enter().append("rect")
+		    						.attr("class", "bar")
+		    						.attr("x", d => {
+		    								return X_SCALE2(d.category) + MARGINS.left
+		    							})
+		    						.attr("y", d => {
+		    							return (Y_SCALE2(d.amount) + MARGINS.bottom)
+		    						})
+		    						.attr("width", X_SCALE2.bandwidth() - 5)
+		    						.attr("height", d => {
+		    							return (VIS_HEIGHT - Y_SCALE2(d.amount))
+		    						})
 
+		// add x-axis
+		// create x-axis
+		FRAME2.append("g")
+	      		.attr("transform", "translate(" + 
+	      			MARGINS.left+ "," + (MARGINS.top + VIS_HEIGHT) + ")")
+	      			.call(d3.axisBottom(X_SCALE2).ticks(10));
+
+		// create y-axis
+		FRAME2.append("g")
+	      		.attr("transform", "translate(" + 
+	      			MARGINS.left + "," + (MARGINS.top) + ")")
+	      		.call(d3.axisLeft(Y_SCALE2).ticks(10));
+
+	    // create a tooltip
+	    const TOOLTIP = d3.select("#col1")
+							.append("div")
+								.attr("class", "tooltip")
+								.style("opacity", 0);
+
+
+		// mouseover event
+		function handleMouseover(event, d) {
+
+			TOOLTIP.style("opacity", 1);
+		}						
+
+		// mousemove event
+		function handleMousemove(event, d) {
+
+			TOOLTIP.html("Name: " + d.category + "<br>Value: " + d.amount)
+					.style("left", (event.pageX + 10) + "px")
+					.style("top", (event.pageY - 50) + "px");
+		}
+
+		// mouseleave event
+		function handleMouseleave(event, d) {
+
+			TOOLTIP.style("opacity", 0);
+		}
+
+		// set the frame with the events
+		FRAME2.selectAll("rect")
+			.on("mouseover", handleMouseover)
+			.on("mousemove", handleMousemove)
+			.on("mouseleave", handleMouseleave);
 
 	});
+}
+
+build_plot();
